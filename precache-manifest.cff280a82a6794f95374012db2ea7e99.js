03d5fff3ea4f9dd2b1db9edf4b33a4c22f57b629
harsha <harshareddy1598@gmail.com>
2020-02-15 23:13:16 +0530

    Updates

---
+++ precache-manifest.cff280a82a6794f95374012db2ea7e99.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d334f8b016d463771caf925f4784e02",
+    "url": "/Monsters-Inc/index.html"
+  },
+  {
+    "revision": "8fc875b09eb4c2800f62",
+    "url": "/Monsters-Inc/static/css/main.e5c7d7ed.chunk.css"
+  },
+  {
+    "revision": "d5b7dc0e95c86de42d59",
+    "url": "/Monsters-Inc/static/js/2.0b9572cb.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/Monsters-Inc/static/js/2.0b9572cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8fc875b09eb4c2800f62",
+    "url": "/Monsters-Inc/static/js/main.33b89b36.chunk.js"
+  },
+  {
+    "revision": "5b686dfb36073e1fce04",
+    "url": "/Monsters-Inc/static/js/runtime-main.f23950c1.js"
+  }
+]);
